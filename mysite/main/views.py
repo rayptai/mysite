f686dc2dfcff604253f7b17427a6e4970194eefe
rayptai <ray.p.tai@gmail.com>
2024-07-11 17:07:18 -0700

    Additional comments for clarity.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -8,9 +8,13 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 def machine_learning_demo(request):
+
     if request.method == 'POST':
+
+        # Handle scenario where the form was posted and we want to make a prediction
         form = FruitForm(request.POST)
 
+        # If the user entered invalid data in the form, return the original form instead of a prediction
         if not form.is_valid():
             form = FruitForm()
             context = {
@@ -18,23 +22,30 @@ def machine_learning_demo(request):
             }
             return render(request, 'ml/ml.html', context)
 
+        # Extract user inputs from the form data
         mass = form.cleaned_data['mass']
         width = form.cleaned_data['width']
         height = form.cleaned_data['height']
 
+        # Read the data that will be used to train the model from disk
         fruits = pd.read_table('./main/data/fruit_data_with_colors.txt')
 
         # create a mapping from fruit label value to fruit name to make results easier to interpret
         lookup_fruit_name = dict(zip(fruits.fruit_label.unique(), fruits.fruit_name.unique()))
 
+        # Split the training data into training and validation data
         x = fruits[['mass', 'width', 'height']]
         y = fruits['fruit_label']
 
         x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
 
+        # Create the machine learning classifier that will be trained
         knn = KNeighborsClassifier(n_neighbors=5)
+
+        # Train the classifier
         knn.fit(x_train, y_train)
 
+        # Make a prediction using the trained classifier
         prediction = knn.predict([[mass, width, height]])
         fruit_name = lookup_fruit_name[prediction[0]]
 
@@ -44,6 +55,8 @@ def machine_learning_demo(request):
 
         return render(request, "ml/ml_result.html", context)
     else:
+
+        # Handle scenario where the user first hit the page that contains the form
         form = FruitForm()
         context = {
             'form': form,
